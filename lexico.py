f1e13261a82d8ed8ad24a8ca580c17f338e7a14b
Eddie Ramirez <djedi.r@gmail.com>
2014-10-21 01:40:48 -0500

    Added generate method

--- lexico.py
+++ lexico.py
@@ -2,8 +2,8 @@ import sys
 import StringIO
 
 class Lexical:
-
-  error_state = 128
+  error_state = -1 
+  accept_state = 128
   raw_string = None
   end_of_file = None
   states = None 
@@ -11,83 +11,78 @@ class Lexical:
   def __init__(self, raw_string):
     self.raw_string = raw_string
     self.states = [
-      # alpha, digit, dot, +, -, _, EOL
-      [2, 3, self.error_state, 2, 2, 2, None]
-      [2, 3, self.error_state, 3, ]
+      # alpha, digit, dot, +, -, *,  _, EOL
+      [1, 3, -1, 4, 4, 4, 4, 4, 4, 1],
+      [128, 128, -1, -1, -1, -1, -1 , -1, -1, 128],
+      [-1, 128, -1, -1, -1, -1, -1, -1, -1, -1],
+      [-1, 128, 128, -1, -1, -1, -1, -1, -1, -1],
+      [-1, -1, -1, 128, 128, 128, 128, 128, 128, -1],
       ]
-    self.analyzer()
+    self.generate()
 
-  def analyzer(self):
+  def generate(self):
     current_state = 0
     previous_state = None 
-    i = 0
-    while current_state != self.accept_state and current_state != self.error_state:
-      for line in StringIO.StringIO(self.raw_string):
-        for char in line:
-          #self.scanner(char)
-          swic
-          
-            case 0 
-            if isdi c_sta=1
-            if isalpha c_sta=2
-            if + c_sta=3
-            if - c_sta=4
-            
-            
-            
-            case 1 if isdi c_sta=1
-            if . c_sta=10
-            
-            case 10
-            if isdi c_sta=11
-            
-            case 11
-            if isdi c_sta=1
-            
-            case 2 
-            if isalpha c_sta=2
-            
-            case 4
-            
-          
-          
-          swich (c_state)
-          
-          case 1 int 
-          case 11 
+    position = 0
+    symbol = None
+    for line in StringIO.StringIO(self.raw_string):
+      for char in line:
+        if current_state != self.accept_state and current_state != self.error_state:
+          position += position
+          if self.is_blank(char):
+            if current_state == 0:
+              continue
+            else:
+              previous_state, current_state = current_state, self.accept_state 
+          if current_state != self.accept_state:
+            symbol = self.char_to_symbol(char)
         
-        
-        
-          
-  def scanner(self, char):
-    if self.isDigit(char): return 1
-    if self.isAlpha(char): return 2
-    if self.isUnderscore(char): return 3
-    if self.isDot(char): return 4
-    
+          if symbol < 0:
+            break
 
-  def isUnderscore(self, char):
+          if symbol >= 0 and symbol <= 9 and current_state != self.accept_state:
+            previous_state, current_state = current_state, self.states[current_state][symbol]
+          print previous_state, current_state
+
+
+        
+        
+          
+  def char_to_symbol(self, char):
+    if self.is_alpha(char): 
+      return 0
+    if self.is_digit(char): 
+      return 1
+    if self.is_dot(char):
+      return 2 
+    if self.is_arithmetic_sum(char): 
+      return 3
+    if self.is_underscore(char): 
+      return 9
+    return -1
+
+  def is_underscore(self, char):
     if char == '_': 
       return True
     return False
 
-  def isDot(self, char):
+  def is_dot(self, char):
     if char == '.':
       return True
     return False
 
-  def isBlank(self, char):
+  def is_blank(self, char):
     if char == ' ' or char == '\t':
       return True
     return False
 
-  def isAlpha(self, char):
+  def is_alpha(self, char):
     return char.isalpha()
 
-  def isDigit(self, char):
+  def is_digit(self, char):
     return char.isdigit()
 
-  def isArithmeticSum(self, char):
+  def is_arithmetic_sum(self, char):
     if char == '+':
       return True
     return False
